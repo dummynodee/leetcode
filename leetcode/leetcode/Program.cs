7d754979c6ba7c6b1939d0eb5bc53fb8598fd95a
gianganderson <gianganderson@Andersons-MacBook-Pro.local>
2018-10-18 13:10:59 -0700

    first commit

---
+++ leetcode/leetcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetcode
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
